222023233bc21c0abde732b9798c8f865a53ab8a
Demiourg <george.alexiou@tutanota.com>
2024-02-26 17:29:50 +0100

    add role animation to index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const buttonEl = document.getElementById("roll-button");
+
+const diceEl = document.getElementById("dice");
+
+buttonEl.addEventListener("click", () => {
+  diceEl.classList.add("roll-animation");
+});
