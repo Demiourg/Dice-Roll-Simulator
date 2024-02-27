8d11b78a07970381a0a30fdecad9f911b814e3c0
Demiourg <george.alexiou@tutanota.com>
2024-02-27 16:10:26 +0100

    add completion of js file

--- index.js
+++ index.js
@@ -2,10 +2,27 @@ const buttonEl = document.getElementById("roll-button");
 
 const diceEl = document.getElementById("dice");
 
+const rollHistoryEl = document.getElementById("roll-history");
+
+let historyList = [];
+
 function rollDice() {
   const rollResult = Math.floor(Math.random() * 6) + 1;
   const diceFace = getDiceFace(rollResult);
   diceEl.innerHTML = diceFace;
+  historyList.push(rollResult);
+  updateRollHistory();
+}
+
+function updateRollHistory() {
+  rollHistoryEl.innerHTML = "";
+  for (let i = 0; i < historyList.length; i++) {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `Roll ${i + 1}: <span>${getDiceFace(
+      historyList[i]
+    )}<span>`;
+    rollHistoryEl.appendChild(listItem);
+  }
 }
 
 function getDiceFace(rollResult) {
